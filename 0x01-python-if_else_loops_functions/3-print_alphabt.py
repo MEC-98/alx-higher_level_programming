27de060ca6d215e0ddee2983127e19543ac5d4c2
Mostafa El Chahmouti <126358648+MEC-98@users.noreply.github.com>
2025-03-13 11:10:35 +0000

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-print("%s" % "".join(chr(i) for i in range(97, 123) if chr(i) not in "qe"), end="")
+for letter in range(97, 123):
+    if chr(letter) not in ('q', 'e'):
+        print("{}".format(chr(letter)), end="")
